2992a0d039905919192012e4038b738a1f53c217
s92i <sofiane.92izii@gmail.com>
2024-11-30 19:21:18 +0100

    Modified experience sorting

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import ExperienceCard from './ExperienceCard'
-import { Experience } from '../typings'
+import React from "react";
+import { motion } from "framer-motion";
+import ExperienceCard from "./ExperienceCard";
+import { Experience } from "../typings";
 
 type Props = {
-    experiences: Experience[]
-}
+  experiences: Experience[];
+};
 
 function WorkExperience({ experiences }: Props) {
-    return (
-        <motion.div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center' initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1.5 }}>
-            <h3 className='absolute top-14 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-            <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-                {experiences.map((experience) => (
-                    <ExperienceCard key={experience._id} experience={experience} />
-                ))}
-            </div>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
+      <h3 className="absolute top-14 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Experience
+      </h3>
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+        {experiences
+          .sort((a, b) => b.dateStarted.localeCompare(a.dateStarted))
+          .map((experience) => (
+            <ExperienceCard key={experience._id} experience={experience} />
+          ))}
+      </div>
+    </motion.div>
+  );
 }
 
-export default WorkExperience
+export default WorkExperience;
